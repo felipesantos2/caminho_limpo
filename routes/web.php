4e3532c8a0f124cdd99624eb3b5a8631bb9bcb4f
felipesantos2 <santospinheiro627@gmail.com>
2026-08-06 12:54:55 -0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/reportes', 'pages.reports.index');
+Route::view('/reportes/{report}/report', 'pages.reports.index');

d1f208b9fddd346e77db1a1b5f1e5e60b2417922
felipesantos2 <santospinheiro627@gmail.com>
2026-08-06 13:45:48 -0300

    wip

--- database/migrations/2026_08_06_145247_create_reports_table.php
+++ database/migrations/2026_08_06_145247_create_reports_table.php
@@ -10,6 +10,12 @@ return new class extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 60);
+            $table->string('content', 1000); // podemos gerar um html para ser rederizado
+            $table->string('media');
+            $table->string('slug', 30);
+            $table->string('url', 30);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
